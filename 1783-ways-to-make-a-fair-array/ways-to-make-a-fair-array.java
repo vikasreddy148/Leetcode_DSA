9d91f1c10d4387208ecf736534ec8705abc0dcbb
vikasreddy148 <119026304+vikasreddy148@users.noreply.github.com>
2025-06-25 11:44:19 +0530

    Time: 5 ms (96.88%) | Memory: 57.8 MB (53.13%) - LeetSync

---
+++ 1783-ways-to-make-a-fair-array/ways-to-make-a-fair-array.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int waysToMakeFair(int[] nums) {
+        int esum = 0;  // Sum of even-indexed elements
+        int osum = 0;  // Sum of odd-indexed elements
+        int n=nums.length;
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                osum += nums[i];
+            } else {
+                esum += nums[i];
+            }
+        }
+        int count = 0;
+        int prev = 0;
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                osum = osum - nums[i] + prev;
+            } else {
+                esum = esum - nums[i] + prev;
+            }
+            if (esum == osum) {
+                count++;
+            }
+            prev = nums[i];
+        }
+        return count;
+    }
+}
