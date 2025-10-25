09f7dab945ad6d05666912af045f19487854214b
Vikas Reddy <kathulavikasr@gmail.com>
2025-10-25 22:49:11 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (44.00%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+         if (n == 0)
+            return 1;
+        if (n < 0) {
+            double t = (myPow(x, -(n / 2)));
+            if (n % 2 == 0) {
+                return 1 / t * 1 / t;
+            } else {
+                return 1 / t * 1 / t * 1 / x;
+            }
+
+        } else {
+            double t1 = myPow(x, n / 2);
+            if (n % 2 == 0) {
+
+                return t1 * t1;
+            } else {
+                return t1 * t1 * x;
+            }
+        }
+
+    }
+};
